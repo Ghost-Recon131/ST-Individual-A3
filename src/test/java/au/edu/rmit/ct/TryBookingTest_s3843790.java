aa08bc164239eff59c7bc4c10c18b350407b592d
Jingxuan Feng <henryfeng24@pm.me>
2021-10-14 00:11:28 +1100

    Completed Check featured events, working on Check cancelled events

--- src/test/java/au/edu/rmit/ct/TryBookingTest_s3843790.java
+++ src/test/java/au/edu/rmit/ct/TryBookingTest_s3843790.java
@@ -13,10 +13,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 
@@ -40,13 +40,36 @@ class TryBookingTest_s3843790 {
     @Order(1)
     @DisplayName("TryBooking: Check featured events")
     void CheckFeaturedEvents() {
-        //TODO
-        // 1. Navigate to https://www.trybooking.com/book/searchLinks print-to-screen the featured event titles.
-        //    Store these titles in a suitable variable in your test case (list? arrays?).
-        // 2. Update your test case to use assertEquals to compare these titles with a new test case run. You are
-        //    expected to show that this test case passes.
-
         final String FeaturedEventsURL = "https://www.trybooking.com/book/searchLinks";
+        final int TotalFeatureEvents = 6;
+        myDriver.get(FeaturedEventsURL);
+        AddDelay(); // 3-second delay was used here, it appeared 1sec was not enough and would result in exceptions
+
+        ArrayList<String> FeatureEventsByXPath = new ArrayList<>(); //Arraylist to store Titles grabbed by XPath
+        ArrayList<String> FeatureEventsByHand = new ArrayList<>(); //Arraylist to store Titles entered by hand
+
+        // Add Featured Events titles using XPath
+        for (int i = 0, j = 1; i < TotalFeatureEvents; i++, j++) {
+            String FeatureEventsXPath = "/html[@class='no-js']/body[@id='body_id']/div[@class='full-height-container ng-scope']" +
+                    "/div[@id='mainContainer']/div[@id='view-id']/div[@class='init-search_wrapper_full ng-scope']/div[@class='init-search_wrapper']/div" +
+                    "[@class='init-search-results']/div[@id='FeaturedEventsSection']/div[@class='init-search-results-parent margin-center no-style']/div" +
+                    "[@class='init-search-results-child ng-scope'][" +
+                    j +
+                    "]/div[@class='init-search-result-container']/div[@class='init-search-result']" +
+                    "/a[@id='initSearchNavigateHeadingFeaturedEventId_" +
+                    i +
+                    "']/h2[@class='ng-binding']";
+            WebElement FeatureEvents = myDriver.findElement(By.xpath(FeatureEventsXPath));
+            FeatureEventsByXPath.add(FeatureEvents.getText());
+        }
+        FeatureEventsByHand.add("That’s Amore - The Dean Martin Story");
+        FeatureEventsByHand.add("BLAK STAGE AT LA MAMA");
+        FeatureEventsByHand.add("Marysville Music Weekend - Rescheduled");
+        FeatureEventsByHand.add("Adventure Travel Film Festival 2021 (V2)");
+        FeatureEventsByHand.add("2022 Port Fairy Jazz Festival");
+        FeatureEventsByHand.add("Dance Hall - The Pheonix Tour");
+
+        assertEquals(FeatureEventsByHand, FeatureEventsByXPath, "Expect arraylists to match");
     }
 
     @Test
@@ -56,8 +79,43 @@ class TryBookingTest_s3843790 {
         //TODO
         // 1. Navigate to https://www.trybooking.com/book/searchLinks as above and check for featured and events near
         //    "you". If any such event has '(Cancelled)' in its title, then this test case should fail.
-
         final String FeaturedEventsURL = "https://www.trybooking.com/book/searchLinks";
+        final int TotalFeatureEvents = 6;
+        final int TotalEventsNearYou = 6;
+        myDriver.get(FeaturedEventsURL);
+        AddDelay();
+
+        ArrayList<String> FeatureEventsByXPathStatus = new ArrayList<>(); //Arraylist to store Titles grabbed by XPath
+
+        for (int i = 0, j = 1; i < TotalFeatureEvents; i++, j++) {
+            String FeatureEventsXPath = "/html[@class='no-js']/body[@id='body_id']/div[@class='full-height-container ng-scope']" +
+                    "/div[@id='mainContainer']/div[@id='view-id']/div[@class='init-search_wrapper_full ng-scope']/div[@class='init-search_wrapper']/div" +
+                    "[@class='init-search-results']/div[@id='FeaturedEventsSection']/div[@class='init-search-results-parent margin-center no-style']/div" +
+                    "[@class='init-search-results-child ng-scope'][" +
+                    j +
+                    "]/div[@class='init-search-result-container']/div[@class='init-search-result']" +
+                    "/a[@id='initSearchNavigateHeadingFeaturedEventId_" +
+                    i +
+                    "']/h2[@class='ng-binding']";
+            WebElement FeatureEvents = myDriver.findElement(By.xpath(FeatureEventsXPath));
+            FeatureEvents.click(); // Go to each event in Featured events
+
+            String EventTitleXPath = "/html/body/div[@id='event-app']/div[@class='full-height-container']/div" +
+                    "[@id='partial-view-content']/div[@id='ko-event-landing']/div[@class='main-container  ']" +
+                    "/div[@class='view-container ']/div[@id='view-id']/div[@class='wrapper ']/div[@class='main-panel']" +
+                    "/div[@class='wrapper-narrow']/div[@class='padding-lr--sm']/div[@id='eventLandingSummary']/div[1]" +
+                    "/span[@class='event-name-container']/span[@class='event-name']";
+            WebElement EventTitle = myDriver.findElement(By.xpath(EventTitleXPath));
+            FeatureEventsByXPathStatus.add(EventTitle.getText()); // Add title to arraylist
+
+            // Return to main site
+            myDriver.get(FeaturedEventsURL);
+            AddDelay();
+        }
+        assertFalse(FeatureEventsByXPathStatus.contains("CANCELLED"), "Test will fail if an event is cancelled");
+
+
+
     }
 
     @Test
@@ -73,7 +131,7 @@ class TryBookingTest_s3843790 {
 
     void AddDelay(){
         try {
-            Thread.sleep(1000);
+            Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
