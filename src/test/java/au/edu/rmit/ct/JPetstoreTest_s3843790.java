a774dded3c07a5184a41b2911b9201c2db9a0950
Jingxuan Feng <henryfeng24@pm.me>
2021-10-13 14:15:01 +1100

    Added test cases to nested class, start work on StartAMenagerie

--- src/test/java/au/edu/rmit/ct/JPetstoreTest_s3843790.java
+++ src/test/java/au/edu/rmit/ct/JPetstoreTest_s3843790.java
@@ -31,132 +31,151 @@ class JPetstoreTest_s3843790 {
         assertEquals("JPetStore Demo", myDriver.getTitle());
     }
 
-    @Test
-    @Order(1)
-    @DisplayName("Task 11.1 : Check that the price is $125.50")
-    void checkChihuahuaPrice() {
+    @Nested
+    class checkChihuahuaNamePriceStock{
+        @Test
+        @Order(1)
+        @DisplayName("Task 11.1 : Check that the price is $125.50")
+        void checkChihuahuaPrice() {
 
-        String chihuahuaURL = "https://petstore.octoperf.com/actions/Catalog.action?viewItem=&itemId=EST-26";
-        myDriver.get(chihuahuaURL);
+            String chihuahuaURL = "https://petstore.octoperf.com/actions/Catalog.action?viewItem=&itemId=EST-26";
+            myDriver.get(chihuahuaURL);
 
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            // Get value using Xpath, then check against expected value
+            WebElement webElement = myDriver.findElement(By.xpath("//td[text()='$125.50']"));
+            assertEquals("$125.50", webElement.getText(), "Should Equal $125.50");
         }
 
-        // Get value using Xpath, then check against expected value
-        WebElement webElement = myDriver.findElement(By.xpath("//td[text()='$125.50']"));
-        assertEquals("$125.50", webElement.getText(), "Should Equal $125.50");
+        @Test
+        @Order(2)
+        @DisplayName("Task 11.2 : Check that the product name is correct (Adult Male Chihuahua) for this product page")
+        void checkChihuahuaProductName() {
+
+            String chihuahuaURL = "https://petstore.octoperf.com/actions/Catalog.action?viewItem=&itemId=EST-26";
+            myDriver.get(chihuahuaURL);
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            WebElement webElement2 = myDriver.findElement(By.xpath("/html/body/div[@id='Content']/div[@id='Catalog']/table/tbody/tr[3]/td/b/font"));
+            assertEquals("Adult Male Chihuahua", webElement2.getText(), "Should Equal Adult Male Chihuahua");
+        }
+
+        @Test
+        @Order(3)
+        @DisplayName("Task 11.3 Check that the adult male chihuahua is in stock. ( > 0 )")
+        void checkChihuahuaStock() {
+
+            String chihuahuaURL = "https://petstore.octoperf.com/actions/Catalog.action?viewItem=&itemId=EST-26";
+            myDriver.get(chihuahuaURL);
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            WebElement webElement3 = myDriver.findElement(By.xpath("/html/body/div[@id='Content']/div[@id='Catalog']/table/tbody/tr[5]/td"));
+
+            // get the string value and only keep the numerical value, then convert to int for final junit comparison
+            String stockOnly = webElement3.getText().replaceAll("[^0-9]", "");
+            int StockInteger = -1;
+            try{
+                StockInteger = Integer.parseInt(stockOnly);
+            }catch (NumberFormatException e){
+                System.err.println("Invalid value, value was " + stockOnly);
+            }
+            boolean hasStock = StockInteger > 0;
+            assertTrue(hasStock);
+        }
+    }
+
+    @Nested
+    class checkAdultMalePersianNamePriceStock{
+        @Test
+        @Order(4)
+        @DisplayName("Task 11.4 : Check that the price is $93.50")
+        void checkAnotherPetPrice() {
+            String chihuahuaURL = "https://petstore.octoperf.com/actions/Catalog.action?viewItem=&itemId=EST-17";
+            myDriver.get(chihuahuaURL);
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            // Get value using Xpath, then check against expected value
+            WebElement webElement = myDriver.findElement(By.xpath("//td[text()='$93.50']"));
+            assertEquals("$93.50", webElement.getText(), "Should Equal 93.50");
+        }
+
+        @Test
+        @Order(5)
+        @DisplayName("Task 11.5 : Check that the product name is correct (Adult Male Persian) for this product page")
+        void checkAnotherPetProductName() {
+
+            String chihuahuaURL = "https://petstore.octoperf.com/actions/Catalog.action?viewItem=&itemId=EST-17";
+            myDriver.get(chihuahuaURL);
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            WebElement webElement2 = myDriver.findElement(By.xpath("/html/body/div[@id='Content']/div[@id='Catalog']/table/tbody/tr[3]/td/b/font"));
+            assertEquals("Adult Male Persian", webElement2.getText(), "Should Equal Adult Male Persian");
+        }
+
+        @Test
+        @Order(6)
+        @DisplayName("Task 11.6 Check that the Adult Male Persian is in stock. ( > 0 )")
+        void checkAnotherPetStock() {
+            String chihuahuaURL = "https://petstore.octoperf.com/actions/Catalog.action?viewItem=&itemId=EST-17";
+            myDriver.get(chihuahuaURL);
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            WebElement webElement3 = myDriver.findElement(By.xpath("/html/body/div[@id='Content']/div[@id='Catalog']/table/tbody/tr[5]/td"));
+
+            // get the string value and only keep the numerical value, then convert to int for final junit comparison
+            String stockOnly = webElement3.getText().replaceAll("[^0-9]", "");
+            int StockInteger = -1;
+            try{
+                StockInteger = Integer.parseInt(stockOnly);
+            }catch (NumberFormatException e){
+                System.err.println("Invalid value, value was " + stockOnly);
+            }
+            boolean hasStock = StockInteger > 0;
+            assertTrue(hasStock);
+        }
     }
 
     @Test
-    @Order(2)
-    @DisplayName("Task 11.2 : Check that the product name is correct (Adult Male Chihuahua) for this product page")
-    void checkChihuahuaProductName() {
+    @Order(7)
+    @DisplayName("StartAMenagerie Task")
+    void StartAMenagerie() {
+        //TODO 3 x fish, 2 x cat, and 1 x bird.
+        // Check the subtotal matches the expected price.
+        final String ToothlessTigerSharkURL = "https://petstore.octoperf.com/actions/Catalog.action?viewItem=&itemId=EST-3";
+        final String AdultMalePersianURL = "https://petstore.octoperf.com/actions/Catalog.action?viewItem=&itemId=EST-17";
+        final String AdultMaleAmazonParrotURL = "https://petstore.octoperf.com/actions/Catalog.action?viewItem=&itemId=EST-17";
+        // Add the 3 sharks to cart:
 
-        String chihuahuaURL = "https://petstore.octoperf.com/actions/Catalog.action?viewItem=&itemId=EST-26";
-        myDriver.get(chihuahuaURL);
-
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        WebElement webElement2 = myDriver.findElement(By.xpath("/html/body/div[@id='Content']/div[@id='Catalog']/table/tbody/tr[3]/td/b/font"));
-        assertEquals("Adult Male Chihuahua", webElement2.getText(), "Should Equal Adult Male Chihuahua");
-    }
-
-    @Test
-    @Order(3)
-    @DisplayName("Task 11.3 Check that the adult male chihuahua is in stock. ( > 0 )")
-    void checkChihuahuaStock() {
-
-        String chihuahuaURL = "https://petstore.octoperf.com/actions/Catalog.action?viewItem=&itemId=EST-26";
-        myDriver.get(chihuahuaURL);
-
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        WebElement webElement3 = myDriver.findElement(By.xpath("/html/body/div[@id='Content']/div[@id='Catalog']/table/tbody/tr[5]/td"));
-
-        // get the string value and only keep the numerical value, then convert to int for final junit comparison
-        String stockOnly = webElement3.getText().replaceAll("[^0-9]", "");
-        int StockInteger = -1;
-        try{
-            StockInteger = Integer.parseInt(stockOnly);
-        }catch (NumberFormatException e){
-            System.err.println("Invalid value, value was " + stockOnly);
-        }
-        boolean hasStock = StockInteger > 0;
-        assertTrue(hasStock);
-    }
-
-    @Test
-    @Order(4)
-    @DisplayName("Task 11.4 : Check that the price is $93.50")
-    void checkAnotherPetPrice() {
-        String chihuahuaURL = "https://petstore.octoperf.com/actions/Catalog.action?viewItem=&itemId=EST-17";
-        myDriver.get(chihuahuaURL);
-
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        // Get value using Xpath, then check against expected value
-        WebElement webElement = myDriver.findElement(By.xpath("//td[text()='$93.50']"));
-        assertEquals("$93.50", webElement.getText(), "Should Equal 93.50");
-    }
-
-    @Test
-    @Order(5)
-    @DisplayName("Task 11.5 : Check that the product name is correct (Adult Male Persian) for this product page")
-    void checkAnotherPetProductName() {
-
-        String chihuahuaURL = "https://petstore.octoperf.com/actions/Catalog.action?viewItem=&itemId=EST-17";
-        myDriver.get(chihuahuaURL);
-
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        WebElement webElement2 = myDriver.findElement(By.xpath("/html/body/div[@id='Content']/div[@id='Catalog']/table/tbody/tr[3]/td/b/font"));
-        assertEquals("Adult Male Persian", webElement2.getText(), "Should Equal Adult Male Persian");
-    }
-
-    @Test
-    @Order(6)
-    @DisplayName("Task 11.6 Check that the Adult Male Persian is in stock. ( > 0 )")
-    void checkAnotherPetStock() {
-        String chihuahuaURL = "https://petstore.octoperf.com/actions/Catalog.action?viewItem=&itemId=EST-17";
-        myDriver.get(chihuahuaURL);
-
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        WebElement webElement3 = myDriver.findElement(By.xpath("/html/body/div[@id='Content']/div[@id='Catalog']/table/tbody/tr[5]/td"));
-
-        // get the string value and only keep the numerical value, then convert to int for final junit comparison
-        String stockOnly = webElement3.getText().replaceAll("[^0-9]", "");
-        int StockInteger = -1;
-        try{
-            StockInteger = Integer.parseInt(stockOnly);
-        }catch (NumberFormatException e){
-            System.err.println("Invalid value, value was " + stockOnly);
-        }
-        boolean hasStock = StockInteger > 0;
-        assertTrue(hasStock);
     }
 
     @BeforeEach
